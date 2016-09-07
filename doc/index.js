46c11dab51c0e5152270e554b3c82aa82cf8880e
William McCall <wcm13@my.fsu.edu>
2016-09-07 14:54:48 -0400

    Deleted old doc folder

--- doc/index.js
+++
@@ -1,7 +0,0 @@
-var screenHandler = new ScreenHandler(document.getElementById("game-canvas"), new ClickActionHandler());
-
-
-screenHandler.addScreen("mainMenu", MainMenu);
-screenHandler.addScreen("game", Game);
-screenHandler.addScreen("victoryScreen", VictoryScreen);
-screenHandler.loadScreen("mainMenu", 3);
